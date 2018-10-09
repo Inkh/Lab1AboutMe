56b6cd8ab974452f7679b34fddb9e63f2e8a9e22
inkh <jhwe92@gmail.com>
2018-10-09 10:59:35 -0700

    Add project files.

---
+++ AboutMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
