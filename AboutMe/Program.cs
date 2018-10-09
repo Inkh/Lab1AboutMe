19d6fd2da16c7dda7437cdadddacbcfecf49a051
inkh <jhwe92@gmail.com>
2018-10-09 12:10:53 -0700

    Created basic methods and WriteLines

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -6,7 +6,31 @@ namespace AboutMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int counter = 0;
+            Console.WriteLine("Hey you! Welcome to my quiz!");
+            Console.WriteLine("It's a fun little questionnaire that tells you a little about myself");
+            Console.WriteLine("So let's get started! Which activity do you think I gravitate towards?");
+
+        }
+
+        static string FavoriteActivity(string answer)
+        {
+            
+        }
+
+        static string FavoriteConsole(int answer)
+        {
+
+        }
+
+        static string FavoriteBag(int answer)
+        {
+
+        }
+
+        static string MyBackground(int answer)
+        {
+
         }
     }
 }
